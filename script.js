228b7cb21278e5d9cbfb7fd557cd7a4350999b15
YungNewton <isaacnewtonahanmisi@gmail.com>
2024-06-08 23:12:30 +0100

    final

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
         submitButton.textContent = 'Processing...';
         submitButton.disabled = true;
 
-        fetch('https://video-processing-backend.onrender.com/upload', {
+        fetch('http://35.178.78.130:8080//upload', {
             method: 'POST',
             body: formData,
             headers: {
